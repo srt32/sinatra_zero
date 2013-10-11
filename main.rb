5903e2a2ef7dbdcb7e0c321a8c178327323ed2be
Simon Taranto <simon.taranto@gmail.com>
2013-10-11 14:17:19 -0600

    complete part two

--- main.rb
+++ main.rb
@@ -1,7 +1,36 @@
 require 'sinatra'
+require 'data_mapper'
 require 'slim'
 
-get '/:task' do
-  @task = params[:task].split('-').join(' ').capitalize
-  slim :task
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.db")
+
+class Task
+  include DataMapper::Resource
+  property :id,      Serial
+  property :name,    String, :required => true
+  property :completed_at, DateTime
 end
+DataMapper.finalize
+
+get '/' do
+  @tasks = Task.all
+  slim :index
+end
+
+post '/' do
+  Task.create  params[:task]
+  redirect to('/')
+end
+
+delete '/task/:id' do
+  Task.get(params[:id]).destroy
+  redirect to('/')
+end
+
+put '/task/:id' do
+  task = Task.get params[:id]
+  task.completed_at = task.completed_at.nil? ? Time.now : nil
+  task.save
+  redirect to('/')
+end
+
