39fe5e8050084e1002fa34d6eadc865d9d92268c
Simon Taranto <simon.taranto@gmail.com>
2013-10-11 15:19:20 -0600

    finish third part

--- main.rb
+++ main.rb
@@ -9,16 +9,25 @@ class Task
   property :id,      Serial
   property :name,    String, :required => true
   property :completed_at, DateTime
+  belongs_to :list
 end
+
+class List
+  include DataMapper::Resource
+  property :id,         Serial
+  property :name,       String, :required => true
+  has n, :tasks, :constraint => :destroy
+end
+
 DataMapper.finalize
 
 get '/' do
-  @tasks = Task.all
+  @lists = List.all(:order => [:name])
   slim :index
 end
 
-post '/' do
-  Task.create  params[:task]
+post '/:id' do
+  List.get(params[:id]).tasks.create params['task']
   redirect to('/')
 end
 
@@ -31,6 +40,16 @@ put '/task/:id' do
   task = Task.get params[:id]
   task.completed_at = task.completed_at.nil? ? Time.now : nil
   task.save
+  redirect to('/')
+end
+
+post '/new/list' do
+  List.create params['list']
+  redirect to('/')
+end
+
+delete '/list/:id' do
+  List.get(params[:id]).destroy
   redirect to('/')
 end
 
